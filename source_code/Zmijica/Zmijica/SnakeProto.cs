fbf175d89459f7b182b3940dda7fd1edb4ee7056
Mihael <miwaque@gmail.com>
2023-12-17 14:39:25 +0100

    Adding minimal load to remote repository using .gitignore file.

---
+++ source_code/Zmijica/Zmijica/SnakeProto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zmijica
+{
+    internal class SnakeProto : IDrawable
+    {
+        (float, float) IDrawable.Position
+        {
+            get => (0, 0);
+        }
+    }
+}
