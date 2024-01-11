00a72c456005fa1bfa0b67b87b349b4bb70a1c36
andrijafromzagreb <durdevicandrija@gmail.com>
2024-01-11 14:11:48 +0100

    osnove su tu

--- source_code/Zmijica/Zmijica/Game.cs
+++ source_code/Zmijica/Zmijica/Game.cs
@@ -14,7 +14,13 @@ namespace Zmijica
         /// Kako bismo osigurali funkcionalnost forme, na početku se 
         /// poziva <see cref="GameForm.GameForm"/>.
         /// </summary>
-        public Game() : base() { }
+        public Game() : base() 
+        {
+            timestamp = 0;
+            foodPosition = new List<Tuple<Point, Food>>();
+            Tuple<Point, Food> newFood = new Tuple <Point, Food>(new Point(7, 7), Food.standard);
+            foodPosition.Insert(0, newFood);
+        }
 
         /*
         U testnom primjeru zmija kreće s (1,1) i kreće se u desno. Ako izađe izvan ekrana se program ruši zbog
@@ -22,9 +28,11 @@ namespace Zmijica
         */
 
         // test data
-        SnakeProto snake;   // testna zmijica
-        Point direction = new Point(1,0);    // testne kontrole
+        Snake snake;   // testna zmijica
+        List<Tuple<Point, Food>> foodPosition;
+        Point direction = new Point(0,0);    // testne kontrole //!mislim da ne treba(init smjer zmijice na 0,0)
         List<Point> pts = new List<Point>();    // sadrži koordinate zida
+        int timestamp;
 
         string[] wallString = new string[] {
             "##########",
@@ -42,10 +50,10 @@ namespace Zmijica
         public override void Setup() 
         {
             InitializeScreen(10);   // za rezoluciju ekrana
-            FPS = 5;    // postavljanje osvježavanja
+            FPS = 2;    // postavljanje osvježavanja
 
-            snake = new SnakeProto(this, 1, 1); // nova zmijica na (5,5)
-            DrawList(snake.pos(), Color.Green); // inicijalno crtanje zmije
+            snake = new Snake(); // nova zmijica na (5,5)
+            DrawList(snake.getPosition(), Color.Green); // inicijalno crtanje zmije
 
             // dekodiranje testnog zida
             for (int i = 0; i < width; i++)
@@ -58,15 +66,54 @@ namespace Zmijica
             DrawList(pts, Color.Purple);  // crta točke iz pts
         }
 
+        //nez jel ovo treba bit neki override
+        private void updateGame()
+        {
+            //TODO: generiraj hranu(dok nije u koliziji sa zmijom ili nekom hranom koja postoji)
+
+            //provjeri je li zmija u koliziji s hranom
+            Point headPosition = snake.headPosition();
+            headPosition.X += direction.X;
+            headPosition.Y += direction.Y;
+            bool hasEaten = false;
+            foreach (Tuple<Point, Food> food in foodPosition.ToList())
+            {
+                if(food.Item1 == headPosition)
+                {
+                    snake.update(direction, food.Item2);
+                    foodPosition.Remove(food);
+                    hasEaten = true;
+                }
+            }
+
+
+
+            // crtanje zmije
+            if (!hasEaten)
+            {
+                snake.update(direction);    // kontrola zmije
+                //foodPosition.RemoveAll(item => item.Item1 == headPosition);
+            }
+        }
+
         public override void Draw() 
         {
+            timestamp++;
+
             // resetiranje pozadine
             ClearScreen();
             DrawList(pts, Color.Purple);  // crta točke iz pts
 
-            // crtanje zmije
-            snake.update(direction);    // kontrola zmije
-            DrawList(snake.pos(), Color.Green);
+            updateGame();
+            //TODO mora se nacrtat svaka vrsta hrane pojedinacno
+            List<Point> standardFood = new List<Point>();
+            foreach(Tuple<Point,Food> food in foodPosition)
+            {
+                if(food.Item2 == Food.standard) standardFood.Insert(0, food.Item1);
+            }
+            DrawList(standardFood, Color.Red);
+
+            DrawList(snake.getPosition(), Color.Green);
         }
 
         public override void KeyPressed() 
---
+++ source_code/Zmijica/Zmijica/Snake.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zmijica
+{
+    internal class Snake
+    {
+        //koristim List i nadam se da tu zadrzi poredak elemenata(first je glava, last je rep)
+        private List<Point> position
+        {
+            get;
+            set;
+        }
+
+        public Point direction
+        {
+            get;
+            set;
+        }
+
+        public Snake(int x = 3, int y = 3)
+        {
+            position = new List<Point>();
+            position.Insert(0, new Point(x, y-1));
+            position.Insert(0, new Point(x, y));
+            direction = new Point(0, 0);
+        }
+
+        public List<Point> getPosition()
+        {
+            return position;
+        }
+
+        public Point headPosition()
+        {
+            return position.First();
+        }
+        public Point tailPosition()
+        {
+            return position.Last();
+        }
+
+        //da se ovo napravi kak treba treba prvo napravit enum za vrste hrane pa prema tome slozit update
+        public void update(Point newDir, Food food = Food.noFood)
+        {
+            if (newDir.X == 0 && newDir.Y == 0) return;
+
+            //za pocetak moze samo genericno, dohvati glavu i smjer i dodaj prikladni element, makni zadnji
+            Point newHeadPosition = this.headPosition();
+            newHeadPosition.X += newDir.X;
+            newHeadPosition.Y += newDir.Y;
+
+            switch (food)
+            {
+                case Food.noFood:
+                    position.Insert(0, newHeadPosition);
+                    position.RemoveAt(position.Count - 1);
+                    break;
+                case Food.standard:
+                    position.Insert(0, newHeadPosition);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
