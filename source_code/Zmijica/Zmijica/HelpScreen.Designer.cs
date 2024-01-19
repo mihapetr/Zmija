b5cf089ccdee625ee8803e43c8d9b64636e19978
JakovMilinkovic <milinjak.math@pmf.hr>
2024-01-19 14:23:00 +0100

    help

--- source_code/Zmijica/Zmijica/HelpScreen.Designer.cs
+++ source_code/Zmijica/Zmijica/HelpScreen.Designer.cs
@@ -31,6 +31,12 @@
             this.buttonPlay = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // buttonPlay
@@ -70,12 +76,75 @@
             this.label1.TabIndex = 22;
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.panel1.Location = new System.Drawing.Point(364, 630);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(23, 22);
+            this.panel1.TabIndex = 23;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Red;
+            this.panel2.Location = new System.Drawing.Point(211, 630);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(23, 22);
+            this.panel2.TabIndex = 24;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Yellow;
+            this.panel3.Location = new System.Drawing.Point(73, 630);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(23, 22);
+            this.panel3.TabIndex = 25;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label2.Location = new System.Drawing.Point(102, 630);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 25);
+            this.label2.TabIndex = 26;
+            this.label2.Text = "- food";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label3.Location = new System.Drawing.Point(240, 630);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 25);
+            this.label3.TabIndex = 27;
+            this.label3.Text = "- poison";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label4.Location = new System.Drawing.Point(393, 630);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 25);
+            this.label4.TabIndex = 28;
+            this.label4.Text = "- powerup";
+            // 
             // HelpScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(584, 681);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.buttonPlay);
@@ -95,5 +164,11 @@
         protected System.Windows.Forms.Button buttonPlay;
         protected System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
--- source_code/Zmijica/Zmijica/HelpScreen.cs
+++ source_code/Zmijica/Zmijica/HelpScreen.cs
@@ -21,25 +21,21 @@ namespace Zmijica
                 "-up:" + "\r\n" +
                 "-left:" + "\r\n" +
                 "-right:" + "\r\n" +
-                "-down:" + "\r\n" +
+                "-down:" + "\r\n" + "\r\n" +
+                "-shift+direction:" + " teleports snake to the nearest wall in that direction" + "\r\n" +
+                "-space+direction:" + " teleports snake to the nearest snake element \n\t in that direction" + "\r\n" +
+                "-n(number)+direction:" + "teleports snake for n tiles in that direction" + "\r\n" +
                 "\r\n\r\n" +
-                "Rules:\r\n1. The snake grows longer by eating food.\r\n" +
+                "Rules:" + "\r\n" + "1. The snake grows longer by eating food.\r\n" +
                 "2. Avoid eating yourself or hitting the walls.\r\n" +
                 "3. Poisonous food shrinks the snake.\r\n" +
                 "4. Food appears randomly on the map.\r\n" +
-                "5. There are 3 stages (maps): 1, 2, 3, 1, 2, 3, ...\r\n" +
-                "6. No level restrictions, but every three stages, difficulty increases.\r\n" +
-                "7. Achieve the goal length to advance to the next stage.\r\n\r\n" +
-                "Tips:\r\n- Pay attention to your snake's length and the stage.\r\n" +
-                "- Be strategic when approaching poisonous food.\r\n" +
+                "5. Achieve the goal length to advance to the next stage.\r\n\r\n" +
+                "Tips:\r\n" +
                 "- Navigate through the stages to reach higher levels.\r\n\r\n" +
                 "Good luck and enjoy playing Snake!" +
-                "\r\n------------------------------------------------------------------------------------\r\n\r\n ";
+                "\r\n------------------------------------------------------------------------------------ ";
             
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
         }
     }
 }
